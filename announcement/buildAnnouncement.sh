2a8900f1646094331cc170b7f406f6c04f5dc047
Nick Elsey <nicholas.elsey@gmail.com>
2019-07-27 15:23:23 -0400

    Initial commit

---
+++ announcement/buildAnnouncement.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+latex announcement.tex
+dvips -t letter announcement
+
+# You can try this if you want to build your announcement in LaTeX. Otherwise just use the Word Document template.
